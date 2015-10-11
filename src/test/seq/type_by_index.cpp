1a64fc38963ff7f6891032f72bd380128ffdc497
Daniel Frey <d.frey@gmx.de>
2015-10-11 17:11:31 +0200

    New

---
+++ src/test/seq/type_by_index.cpp
@@ -0,0 +1,25 @@
+// The Art of C++ / Sequences
+// Copyright (c) 2015 Daniel Frey
+
+#include <tao/seq/type_by_index.hpp>
+
+#include <type_traits>
+
+int main()
+{
+  using namespace tao::seq;
+
+  static_assert( std::is_same< type_by_index_t< 0, int >, int >::value, "oops" );
+
+  static_assert( !std::is_same< type_by_index_t< 0, void, int >, int >::value, "oops" );
+  static_assert( std::is_same< type_by_index_t< 1, void, int >, int >::value, "oops" );
+
+  static_assert( !std::is_same< type_by_index_t< 53,
+                 void, void, void, void, void, void, void, void,
+                 void, void, void, void, void, void, void, void,
+                 void, void, void, void, void, void, void, void,
+                 void, void, void, void, void, void, void, void,
+                 void, void, void, void, void, void, void, void,
+                 void, void, void, void, void, void, void, void,
+                 void, void, void, void, long, void, void, void >, long >::value, "oops" );
+}
