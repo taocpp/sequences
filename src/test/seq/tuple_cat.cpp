ae5b28c5828bf8fbf8934ccc4ef77a5e4fdbd407
Daniel Frey <d.frey@gmx.de>
2015-10-17 11:32:18 +0200

    Test that empty tuples are 'skipped'

--- src/test/seq/tuple_cat.cpp
+++ src/test/seq/tuple_cat.cpp
@@ -10,6 +10,7 @@ int main()
 {
   auto t = tao::seq::tuple_cat( std::make_tuple( 1, true ),
                                 std::make_tuple( 1.0 ),
+                                std::make_tuple(),
                                 std::make_tuple( 1UL, nullptr ) );
 
   static_assert( std::is_same< decltype( t ), std::tuple< int, bool, double, unsigned long, std::nullptr_t > >::value, "oops" );
