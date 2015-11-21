57a41b57178b8c24d3f31e0155a5424e7d6034d7
Daniel Frey <d.frey@gmx.de>
2015-11-21 10:15:35 +0100

    Simplify, make impl::generate_range more powerful

--- include/tao/seq/make_integer_range.hpp
+++ include/tao/seq/make_integer_range.hpp
@@ -12,24 +12,18 @@ namespace tao
   {
     namespace impl
     {
-      template< typename T, T N, T M, bool D = ( M < N ), typename = make_integer_sequence< T, D ? ( N - M ) : ( M - N ) > >
+      template< typename T, T Begin, T Steps, T Delta, typename = make_integer_sequence< T, Steps > >
       struct generate_range;
 
-      template< typename T, T N, T M, T... Ns >
-      struct generate_range< T, N, M, false, integer_sequence< T, Ns... > >
+      template< typename T, T B, T S, T D, T... Ns >
+      struct generate_range< T, B, S, D, integer_sequence< T, Ns... > >
       {
-        using type = integer_sequence< T, N + Ns... >;
-      };
-
-      template< typename T, T N, T M, T... Ns >
-      struct generate_range< T, N, M, true, integer_sequence< T, Ns... > >
-      {
-        using type = integer_sequence< T, N - Ns... >;
+        using type = integer_sequence< T, B + D * Ns... >;
       };
     }
 
     template< typename T, T N, T M >
-    using make_integer_range = typename impl::generate_range< T, N, M >::type;
+    using make_integer_range = typename impl::generate_range< T, N, ( N <= M ) ? ( M - N ) : ( N - M ), ( N <= M ) ? 1 : -1 >::type;
 
     template< std::size_t N, std::size_t M >
     using make_index_range = make_integer_range< std::size_t, N, M >;
