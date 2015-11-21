72a2019ac251a7a050bfc423affd50eb969cae79
Daniel Frey <d.frey@gmx.de>
2015-11-21 10:30:39 +0100

    Fixed warning about negative unsigned values

--- include/tao/seq/make_integer_range.hpp
+++ include/tao/seq/make_integer_range.hpp
@@ -12,18 +12,24 @@ namespace tao
   {
     namespace impl
     {
-      template< typename T, T Begin, T Steps, T Delta, typename = make_integer_sequence< T, Steps > >
+      template< typename T, T Begin, T Steps, bool Increase, T Delta = T( 1 ), typename = make_integer_sequence< T, Steps > >
       struct generate_range;
 
       template< typename T, T B, T S, T D, T... Ns >
-      struct generate_range< T, B, S, D, integer_sequence< T, Ns... > >
+      struct generate_range< T, B, S, true, D, integer_sequence< T, Ns... > >
       {
         using type = integer_sequence< T, B + D * Ns... >;
+      };
+
+      template< typename T, T B, T S, T D, T... Ns >
+      struct generate_range< T, B, S, false, D, integer_sequence< T, Ns... > >
+      {
+        using type = integer_sequence< T, B - D * Ns... >;
       };
     }
 
     template< typename T, T N, T M >
-    using make_integer_range = typename impl::generate_range< T, N, ( N <= M ) ? ( M - N ) : ( N - M ), ( N <= M ) ? 1 : -1 >::type;
+    using make_integer_range = typename impl::generate_range< T, N, ( N <= M ) ? ( M - N ) : ( N - M ), ( N <= M ) >::type;
 
     template< std::size_t N, std::size_t M >
     using make_index_range = make_integer_range< std::size_t, N, M >;
