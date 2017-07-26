e57f3e5e260ac509c9566ed44c0ab2c3395fb489
Daniel Frey <d.frey@gmx.de>
2017-07-26 22:53:41 +0200

    Simplify code, improve efficiency

--- include/tao/seq/make_integer_sequence.hpp
+++ include/tao/seq/make_integer_sequence.hpp
@@ -24,52 +24,55 @@ namespace tao
 
 #else
 
-      // idea from http://stackoverflow.com/a/13073076
-
       namespace impl
       {
-         template< typename, std::size_t, bool >
-         struct double_up;
-
-         template< typename T, T... Ns, std::size_t N >
-         struct double_up< integer_sequence< T, Ns... >, N, false >
-         {
-            using type = integer_sequence< T, Ns..., ( N + Ns )... >;
-         };
-
-         template< typename T, T... Ns, std::size_t N >
-         struct double_up< integer_sequence< T, Ns... >, N, true >
-         {
-            using type = integer_sequence< T, Ns..., ( N + Ns )..., 2 * N >;
-         };
-
-         template< typename T, T N, typename = void >
+         // we have four instantiations of generate_sequence<>, independent of T or N.
+         // V is the current bit, E is the end marker - if true, this is the last step.
+         template< bool V, bool E >
          struct generate_sequence;
 
-         template< typename T, T N >
-         using generate_sequence_t = typename generate_sequence< T, N >::type;
-
-         template< typename T, T N, typename >
-         struct generate_sequence
-            : double_up< generate_sequence_t< T, N / 2 >, N / 2, N % 2 == 1 >
+         // last step: generate final integer sequence
+         template<>
+         struct generate_sequence< false, true >
          {
+            template< typename T, T M, T N, std::size_t S, T... Ns >
+            using f = integer_sequence< T, Ns... >;
          };
 
-         template< typename T, T N >
-         struct generate_sequence< T, N, typename std::enable_if< ( N == 0 ) >::type >
+         template<>
+         struct generate_sequence< true, true >
          {
-            using type = integer_sequence< T >;
+            template< typename T, T M, T N, std::size_t S, T... Ns >
+            using f = integer_sequence< T, Ns..., S >;
          };
 
-         template< typename T, T N >
-         struct generate_sequence< T, N, typename std::enable_if< ( N == 1 ) >::type >
+         // intermediate step: double existing values, append one more if V is set.
+         template<>
+         struct generate_sequence< false, false >
          {
-            using type = integer_sequence< T, 0 >;
+            template< typename T, T M, T N, std::size_t S, T... Ns >
+            using f = typename generate_sequence< ( N & ( M / 2 ) ) != 0, ( M / 2 ) == 0 >::template f< T, M / 2, N, 2 * S, Ns..., ( Ns + S )... >;
+         };
+
+         template<>
+         struct generate_sequence< true, false >
+         {
+            template< typename T, T M, T N, std::size_t S, T... Ns >
+            using f = typename generate_sequence< ( N & ( M / 2 ) ) != 0, ( M / 2 ) == 0 >::template f< T, M / 2, N, 2 * S + 1, Ns..., ( Ns + S )..., 2 * S >;
+         };
+
+         // the final sequence per T/N should be memoized, it will probably be used multiple times.
+         // also checks the limit and starts the above generator properly.
+         template< typename T, T N >
+         struct memoize_sequence
+         {
+            static_assert( N < T( 1 << 20 ), "N too large" );
+            using type = typename generate_sequence< false, false >::template f< T, ( N < T( 1 << 10 ) ) ? T( 1 << 10 ) : T( 1 << 20 ), N, 0 >;
          };
       }
 
       template< typename T, T N >
-      using make_integer_sequence = impl::generate_sequence_t< T, N >;
+      using make_integer_sequence = typename impl::memoize_sequence< T, N >::type;
 
       template< std::size_t N >
       using make_index_sequence = make_integer_sequence< std::size_t, N >;
