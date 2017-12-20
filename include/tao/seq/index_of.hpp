ac4771d5b3c2f2c529f5d667bf8f0173c2220343
Daniel Frey <d.frey@gmx.de>
2017-12-20 16:14:14 +0100

    New

---
+++ include/tao/seq/index_of.hpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2017 Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/sequences/
+
+#ifndef TAOCPP_SEQUENCES_INCLUDE_INDEX_OF_HPP
+#define TAOCPP_SEQUENCES_INCLUDE_INDEX_OF_HPP
+
+#include "contains.hpp"
+#include "integer_sequence.hpp"
+#include "make_integer_sequence.hpp"
+#include "min.hpp"
+#include "sequence_helper.hpp"
+
+namespace tao
+{
+   namespace seq
+   {
+      namespace impl
+      {
+         template< typename, typename >
+         struct index_of;
+
+         template< typename U, std::size_t... Is >
+         struct index_of< U, index_sequence< Is... > >
+         {
+            template< typename T, T N, T... Ns >
+            using type = seq::min< std::size_t, ( ( N == Ns ) ? Is : sizeof...( Is ) )... >;
+         };
+
+         template< typename T, T... Ns, std::size_t... Is >
+         struct index_of< integer_sequence< T, Ns... >, index_sequence< Is... > >
+         {
+            template< typename, T N >
+            using type = seq::min< std::size_t, ( ( N == Ns ) ? Is : sizeof...( Is ) )... >;
+         };
+      }
+
+      template< typename T, typename impl::element_type< T >::type N, typename impl::element_type< T >::type... Ns >
+      struct index_of
+         : impl::index_of< T, make_index_sequence< impl::sequence_size< T, Ns... >::value > >::template type< T, N, Ns... >
+      {
+         static_assert( contains< T, N, Ns... >::value, "index not found" );
+      };
+   }
+}
+
+#endif  // TAOCPP_SEQUENCES_INCLUDE_INDEX_OF_HPP
