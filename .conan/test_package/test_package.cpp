b26f335fcdd36702cde0696004d29fe83b0ce3cb
Daniel Frey <d.frey@gmx.de>
2018-06-25 21:45:03 +0200

    Add Conan support

---
+++ .conan/test_package/test_package.cpp
@@ -0,0 +1,16 @@
+// The Art of C++ / Sequences
+// Copyright (c) 2013-2018 Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/operators/
+
+#include <tao/seq/exclusive_scan.hpp>
+#include <tao/seq/integer_sequence.hpp>
+
+#include <type_traits>
+
+int main()
+{
+   using namespace tao::seq;
+
+   static_assert( std::is_same< exclusive_scan_t< unsigned >, integer_sequence< unsigned > >::value, "oops" );
+   static_assert( std::is_same< exclusive_scan_t< int, 2, 3, -1, 0, 4, -1, 1, 1 >, integer_sequence< int, 0, 2, 5, 4, 4, 8, 7, 8 > >::value, "oops" );
+}
