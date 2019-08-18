5dba9f392e364bf573b6d8260d3ecfa84b73b969
Daniel Frey <d.frey@gmx.de>
2019-08-18 09:58:55 +0200

    Massiv improvement of compile-time efficiency

--- include/tao/seq/sort.hpp
+++ include/tao/seq/sort.hpp
@@ -6,6 +6,7 @@
 
 #include <type_traits>
 
+#include "at_index.hpp"
 #include "integer_sequence.hpp"
 #include "sequence_helper.hpp"
 
@@ -15,35 +16,56 @@ namespace tao
    {
       namespace impl
       {
-         template< typename OP, typename T, T V, typename S, typename R >
-         struct insert;
+         template< typename OP, typename A, typename B, typename R >
+         struct merge;
 
-         template< typename OP, typename T, T V, T... Rs >
-         struct insert< OP, T, V, integer_sequence< T >, integer_sequence< T, Rs... > >
+         template< typename OP, typename T, T A, T... As, T... Rs >
+         struct merge< OP, integer_sequence< T, A, As... >, integer_sequence< T >, integer_sequence< T, Rs... > >
          {
-            using type = integer_sequence< T, Rs..., V >;
+            using type = integer_sequence< T, Rs..., A, As... >;
          };
 
-         template< typename OP, typename T, T V, T N, T... Ns, T... Rs >
-         struct insert< OP, T, V, integer_sequence< T, N, Ns... >, integer_sequence< T, Rs... > >
+         template< typename OP, typename T, T B, T... Bs, T... Rs >
+         struct merge< OP, integer_sequence< T >, integer_sequence< T, B, Bs... >, integer_sequence< T, Rs... > >
          {
-            using type = typename conditional< OP::template apply< T, V, N >::value >::template type<
-               integer_sequence< T, Rs..., V, N, Ns... >,
-               typename insert< OP, T, V, integer_sequence< T, Ns... >, integer_sequence< T, Rs..., N > >::type >;
+            using type = integer_sequence< T, Rs..., B, Bs... >;
          };
 
-         template< typename OP, typename Sorted, typename Unsorted >
-         struct sort;
-
-         template< typename OP, typename T, T... Ns >
-         struct sort< OP, integer_sequence< T, Ns... >, integer_sequence< T > >
+         template< typename OP, typename T, T A, T... As, T B, T... Bs, T... Rs >
+         struct merge< OP, integer_sequence< T, A, As... >, integer_sequence< T, B, Bs... >, integer_sequence< T, Rs... > >
+            : conditional< OP::template apply< T, A, B >::value >::template type<
+                 merge< OP, integer_sequence< T, As... >, integer_sequence< T, B, Bs... >, integer_sequence< T, Rs..., A > >,
+                 merge< OP, integer_sequence< T, A, As... >, integer_sequence< T, Bs... >, integer_sequence< T, Rs..., B > > >
          {
-            using type = integer_sequence< T, Ns... >;
          };
 
-         template< typename OP, typename T, T... Ns, T V, T... Rs >
-         struct sort< OP, integer_sequence< T, Ns... >, integer_sequence< T, V, Rs... > >
-            : sort< OP, typename insert< OP, T, V, integer_sequence< T, Ns... >, integer_sequence< T > >::type, integer_sequence< T, Rs... > >
+         template< typename OP, typename T, bool, typename I, typename... Ss >
+         struct multi_merge;
+
+         template< typename OP, typename T, typename S >
+         struct multi_merge< OP, T, false, index_sequence<>, S >
+         {
+            using type = S;
+         };
+
+         template< typename OP, typename T, std::size_t... Is, typename... Ss >
+         struct multi_merge< OP, T, true, index_sequence< Is... >, Ss... >
+            : multi_merge< OP,
+                           T,
+                           ( ( sizeof...( Is ) % 2 ) == 0 ),
+                           make_index_sequence< sizeof...( Is ) / 2 >,
+                           typename merge< OP, at_index_t< Is * 2, Ss... >, at_index_t< Is * 2 + 1, Ss... >, integer_sequence< T > >::type... >
+         {
+         };
+
+         template< typename OP, typename T, std::size_t... Is, typename S, typename... Ss >
+         struct multi_merge< OP, T, false, index_sequence< Is... >, S, Ss... >
+            : multi_merge< OP,
+                           T,
+                           ( ( ( sizeof...( Is ) + 1 ) % 2 ) == 0 ),
+                           make_index_sequence< ( sizeof...( Is ) + 1 ) / 2 >,
+                           S,
+                           typename merge< OP, at_index_t< Is * 2, Ss... >, at_index_t< Is * 2 + 1, Ss... >, integer_sequence< T > >::type... >
          {
          };
 
@@ -51,8 +73,18 @@ namespace tao
 
       template< typename OP, typename T, T... Ns >
       struct sort
-         : impl::sort< OP, integer_sequence< T >, integer_sequence< T, Ns... > >
+         : impl::multi_merge< OP,
+                              T,
+                              ( ( sizeof...( Ns ) % 2 ) == 0 ),
+                              make_index_sequence< sizeof...( Ns ) / 2 >,
+                              integer_sequence< T, Ns >... >
       {
+      };
+
+      template< typename OP, typename T >
+      struct sort< OP, T >
+      {
+         using type = integer_sequence< T >;
       };
 
       template< typename OP, typename T, T... Ns >
