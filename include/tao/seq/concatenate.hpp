9e0f03a26d7373b41d31c3e8a4e9f610f9b6ccd0
Daniel Frey <d.frey@gmx.de>
2019-08-24 11:07:53 +0200

    Use C++17 fold expression when possible

--- include/tao/seq/concatenate.hpp
+++ include/tao/seq/concatenate.hpp
@@ -6,12 +6,48 @@
 
 #include <type_traits>
 
+#include "config.hpp"
 #include "integer_sequence.hpp"
 
 namespace tao
 {
    namespace seq
    {
+
+#ifdef TAO_SEQ_FOLD_EXPRESSIONS
+
+      namespace impl
+      {
+         template< typename T >
+         struct wrap_concat
+         {
+         };
+
+         template< typename TA, TA... As, typename TB, TB... Bs >
+         constexpr auto operator+( integer_sequence< TA, As... >, wrap_concat< integer_sequence< TB, Bs... > > ) noexcept
+         {
+            return integer_sequence< typename std::common_type< TA, TB >::type, As..., Bs... >();
+         }
+
+         template< typename T, typename... Ts >
+         constexpr auto concat() noexcept
+         {
+            return ( T() + ... + wrap_concat< Ts >() );
+         }
+
+      }  // namespace impl
+
+      template< typename... Ts >
+      using concatenate_t = decltype( impl::concat< Ts... >() );
+
+      template< typename... Ts >
+      struct concatenate
+      {
+         using type = concatenate_t< Ts... >;
+      };
+
+#else
+
       template< typename... >
       struct concatenate;
 
@@ -21,6 +57,7 @@ namespace tao
          using type = integer_sequence< T, Ns... >;
       };
 
+      // TODO: Improve recursion
       template< typename TA, TA... As, typename TB, TB... Bs, typename... Ts >
       struct concatenate< integer_sequence< TA, As... >, integer_sequence< TB, Bs... >, Ts... >
          : concatenate< integer_sequence< typename std::common_type< TA, TB >::type, As..., Bs... >, Ts... >
@@ -29,6 +66,8 @@ namespace tao
 
       template< typename... Ts >
       using concatenate_t = typename concatenate< Ts... >::type;
+
+#endif
 
    }  // namespace seq
 
