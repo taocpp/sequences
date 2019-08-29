11b76165e2f0c75b3db98cdb24c9ce0900bb66c8
Daniel Frey <d.frey@gmx.de>
2019-08-29 23:46:01 +0200

    Fix compilation with GCC for pre-C++17

--- include/tao/seq/exclusive_scan.hpp
+++ include/tao/seq/exclusive_scan.hpp
@@ -13,64 +13,80 @@ namespace tao
 {
    namespace seq
    {
-#if( __cplusplus >= 201402L )
-
       namespace impl
       {
+
+#if( __cplusplus >= 201402L )
+
          template< typename OP, typename T, T... Ns, T V >
-         constexpr auto exclusive_scan( integer_sequence< T, Ns... >, std::integral_constant< T, V >, integer_sequence< T > ) noexcept
+         constexpr auto exclusive_impl( integer_sequence< T, Ns... >, std::integral_constant< T, V >, integer_sequence< T > ) noexcept
          {
             return integer_sequence< T, Ns... >();
          }
 
          template< typename OP, typename T, T... Ns, T V, T R, T... Rs >
-         constexpr auto exclusive_scan( integer_sequence< T, Ns... >, std::integral_constant< T, V >, integer_sequence< T, R, Rs... > ) noexcept
+         constexpr auto exclusive_impl( integer_sequence< T, Ns... >, std::integral_constant< T, V >, integer_sequence< T, R, Rs... > ) noexcept
          {
-            return exclusive_scan< OP, T >( integer_sequence< T, Ns..., V >(), typename OP::template apply< T, V, R >(), integer_sequence< T, Rs... >() );
+            return exclusive_impl< OP, T >( integer_sequence< T, Ns..., V >(), typename OP::template apply< T, V, R >(), integer_sequence< T, Rs... >() );
          }
 
-      }  // namespace impl
+         template< typename T >
+         struct exclusive_scan
+         {
+            template< typename OP, T Init, T... Ns >
+            struct apply
+            {
+               using type = decltype( exclusive_impl< OP, T >( integer_sequence< T >(), std::integral_constant< T, Init >(), integer_sequence< T, Ns... >() ) );
+            };
+         };
 
-      template< typename OP, typename T, typename impl::element_type< T >::type Init, T... Ns >
-      struct exclusive_scan
-      {
-         using type = decltype( impl::exclusive_scan< OP, T >( integer_sequence< T >(), std::integral_constant< T, Init >(), integer_sequence< T, Ns... >() ) );
-      };
+         template< typename T, T... Ns >
+         struct exclusive_scan< integer_sequence< T, Ns... > >
+         {
+            template< typename OP, T Init >
+            struct apply
+            {
+               using type = decltype( exclusive_impl< OP, T >( integer_sequence< T >(), std::integral_constant< T, Init >(), integer_sequence< T, Ns... >() ) );
+            };
+         };
 
 #else
 
-      namespace impl
-      {
          template< typename, typename T, typename, T, typename >
-         struct exclusive_scan;
+         struct exclusive_impl;
 
          template< typename OP, typename T, T... Ns, T V >
-         struct exclusive_scan< OP, T, integer_sequence< T, Ns... >, V, integer_sequence< T > >
+         struct exclusive_impl< OP, T, integer_sequence< T, Ns... >, V, integer_sequence< T > >
          {
             using type = integer_sequence< T, Ns... >;
          };
 
          template< typename OP, typename T, T... Ns, T V, T R, T... Rs >
-         struct exclusive_scan< OP, T, integer_sequence< T, Ns... >, V, integer_sequence< T, R, Rs... > >
-            : exclusive_scan< OP, T, integer_sequence< T, Ns..., V >, OP::template apply< T, V, R >::value, integer_sequence< T, Rs... > >
+         struct exclusive_impl< OP, T, integer_sequence< T, Ns... >, V, integer_sequence< T, R, Rs... > >
+            : exclusive_impl< OP, T, integer_sequence< T, Ns..., V >, OP::template apply< T, V, R >::value, integer_sequence< T, Rs... > >
          {
          };
+
+         template< typename T >
+         struct exclusive_scan
+         {
+            template< typename OP, T Init, T... Ns >
+            using apply = exclusive_impl< OP, T, integer_sequence< T >, Init, integer_sequence< T, Ns... > >;
+         };
+
+         template< typename T, T... Ns >
+         struct exclusive_scan< integer_sequence< T, Ns... > >
+         {
+            template< typename OP, T Init >
+            using apply = exclusive_impl< OP, T, integer_sequence< T >, Init, integer_sequence< T, Ns... > >;
+         };
+
+#endif
 
       }  // namespace impl
 
       template< typename OP, typename T, typename impl::element_type< T >::type Init, T... Ns >
-      struct exclusive_scan
-         : impl::exclusive_scan< OP, T, integer_sequence< T >, Init, integer_sequence< T, Ns... > >
-      {
-      };
-
-#endif
-
-      template< typename OP, typename T, typename impl::element_type< T >::type Init, T... Ns >
-      struct exclusive_scan< OP, integer_sequence< T, Ns... >, Init >
-         : exclusive_scan< OP, T, Init, Ns... >
-      {
-      };
+      using exclusive_scan = typename impl::exclusive_scan< T >::template apply< OP, Init, Ns... >;
 
       template< typename OP, typename T, typename impl::element_type< T >::type Init, T... Ns >
       using exclusive_scan_t = typename exclusive_scan< OP, T, Init, Ns... >::type;
